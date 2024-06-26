6236c2254860ad3206e670a004e4c97efab0dcc2
Mujahid2000 <your.email@example.com>
2024-06-27 01:29:39 +0600

    add many code

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res) =>{
+    res.send('running server')
+})
+
+app.listen(port, () =>{
+    console.log(`server is running on port ${port}`);
+})
